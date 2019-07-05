1148d176609ebfb03b65c46107c91a2f1da7f093
foxman <lin.vladimir@gmail.com>
2019-07-06 07:31:57 +0800

    First Version Of CIOS For JavaScript

---
+++ CIOS.js
@@ -0,0 +1,61 @@
+var CommonAjaxError = function ( xhr , ajaxOptions , thrownError )
+{
+  alert ( thrownError + " => " + ajaxOptions ) ;
+}
+
+var ReportAjaxProblem = function ( data )
+{
+  alert ( data [ "Problem" ] ) ;
+}
+
+var SilenceAJAX = function ( data )
+{
+  var tzHtml = data [ "Answer" ] ;
+  if ( tzHtml === 'Yes' ) {
+  } else {
+    ReportAjaxProblem ( data ) ;
+  } ;
+}
+
+var NormalAJAX = function ( URL , DATA , FUNC , ErrorFunction )
+{
+  $.ajax({
+    url: URL ,
+    type: "POST",
+    cache: false,
+    async: false,
+    dataType: 'json',
+    data: DATA,
+    success: FUNC,
+    error: ErrorFunction,
+  });
+}
+
+var AssignAJAX = function ( URL , DATA )
+{
+  NormalAJAX ( URL , DATA , SilenceAJAX , CommonAjaxError ) ;
+}
+
+var CommonAJAX = function ( URL , DATA , FUNC )
+{
+  NormalAJAX ( URL , DATA , FUNC , CommonAjaxError ) ;
+}
+
+var FetchByAJAX = function ( URL , DATA , Section )
+{
+  $.ajax({
+    url: URL ,
+    type: "POST",
+    cache: false,
+    async: false,
+    dataType: 'json',
+    data: DATA,
+    success: function ( data ) {
+      var tzHtml = data [ "Answer" ] ;
+      if ( tzHtml === "Yes" ) {
+        $( Section ) . html ( data [ "Message" ] ) ;
+      } else {
+      }
+    }
+  });
+}
