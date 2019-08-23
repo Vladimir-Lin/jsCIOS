dafec0d2b97d9abd8a0a788b843d58ee04f4e301
foxman <lin.vladimir@gmail.com>
2019-08-23 23:15:02 +0800

    add tabulator locales

---
+++ locales.js
@@ -0,0 +1,34 @@
+var TabulatorLocales = {
+  "en": {
+          "ajax":       {
+                          "loading": "Loading", //ajax loader text
+                          "error": "Error", //ajax error text
+                        } ,
+          "pagination": {
+                          "first": "First", //text for the first page button
+                          "first_title": "First Page", //tooltip text for the first page button
+                          "last": "Last",
+                          "last_title": "Last Page",
+                          "prev": "Prev",
+                          "prev_title": "Prev Page",
+                          "next": "Next",
+                          "next_title": "Next Page",
+                        } ,
+        } ,
+  "zh-TW": {
+          "ajax":       {
+                          "loading": "載入中" ,
+                          "error": "發生錯誤" ,
+                        } ,
+          "pagination": {
+                          "first": "首頁" ,
+                          "first_title": "第一頁", //tooltip text for the first page button
+                          "last": "尾頁",
+                          "last_title": "最後一頁",
+                          "prev": "上一頁",
+                          "prev_title": "上一頁",
+                          "next": "下一頁",
+                          "next_title": "下一頁",
+                        } ,
+        } ,
+} ;
