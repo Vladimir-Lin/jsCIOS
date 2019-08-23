1472b801e78fdd40f9773e66c787f780016e8dbb
foxman <lin.vladimir@gmail.com>
2019-08-24 00:31:00 +0800

    update locales

--- locales.js
+++ locales.js
@@ -21,14 +21,15 @@ var TabulatorLocales = {
                           "error": "發生錯誤" ,
                         } ,
           "pagination": {
+                          "page_size": "分頁筆數：" ,
                           "first": "首頁" ,
-                          "first_title": "第一頁", //tooltip text for the first page button
-                          "last": "尾頁",
-                          "last_title": "最後一頁",
-                          "prev": "上一頁",
-                          "prev_title": "上一頁",
-                          "next": "下一頁",
-                          "next_title": "下一頁",
+                          "first_title": "第一頁" , //tooltip text for the first page button
+                          "last": "尾頁" ,
+                          "last_title": "最後一頁" ,
+                          "prev": "上一頁" ,
+                          "prev_title": "上一頁" ,
+                          "next": "下一頁" ,
+                          "next_title": "下一頁" ,
                         } ,
         } ,
 } ;
