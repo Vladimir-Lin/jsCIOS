12166a8b6e53e36899592adf40c9a876a4a5890a
foxman <lin.vladimir@gmail.com>
2019-12-16 08:47:44 +0800

    Commit Cios Javascript

--- CIOS.js
+++ CIOS.js
@@ -182,6 +182,37 @@ var SetSession = function ( tag , value )
   return answer ;
 }
 
+var setCookie = function ( name , value , days )
+{
+  var expires = "";
+  if ( days ) {
+    var d = new Date();
+    d . setTime ( d . getTime() + ( days * 24 * 60 * 60 * 1000 ) ) ;
+    expires = "; expires=" + d . toUTCString ( ) ;
+  }
+  document.cookie = name + "=" + ( value || "" )  + expires + "; path=/";
+}
+
+var getCookie = function ( name )
+{
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  var i ;
+  for ( i = 0 ; i < ca . length ; i++ ) {
+    var c = ca [ i ] ;
+    while (  c . charAt ( 0 ) == ' ' ) c = c.substring(1,c.length);
+    if ( c . indexOf ( nameEQ ) == 0) {
+      return c . substring ( nameEQ . length , c . length ) ;
+    }
+  }
+  return null ;
+}
+
+var eraseCookie = function ( name )
+{
+  document . cookie = name + '=; Max-Age=-99999999;' ;
+}
+
 var PurgeInput = function ( input )
 {
   input = input . replace ( "\t" , "" ) ;
